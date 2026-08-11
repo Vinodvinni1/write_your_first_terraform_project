3651c0e7e931641e02ff9239f19da3175a7de32d
Vinodvinni1 <vinodvinni.007@gmail.com>
2026-08-11 14:33:34 +0530

    Updated AMI Id

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = "t2.micro"
 
   tags = {

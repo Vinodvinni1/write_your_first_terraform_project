f91b197841d9bed807fbf3c2ab54018e3e214159
Vinodvinni1 <vinodvinni.007@gmail.com>
2026-08-11 19:19:45 +0530

    adding VPC

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,4 +1,4 @@
-#sample files here
+
 terraform {
   required_providers {
     aws = {
@@ -13,20 +13,72 @@ terraform {
 provider "aws" {
   region  = var.aws_region
 }
-
+# EC2 Instance 
 resource "aws_instance" "app_server" {
   ami           = var.ami_id 
   instance_type = var.instance_type
+  subnet_id     = aws_subnet.public_subnet.id
 
   tags = {
     Name = var.ec2_name_tag
   }
 }
-
+# S3 Bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.s3_bucket_name
 
   tags = {
     Name        = var.s3_name_tag
   }
+}
+# VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my-small-vpc"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
 }
